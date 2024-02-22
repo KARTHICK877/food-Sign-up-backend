cb6513fa879da5233db170c8deba6d9d03cb8eef
KARTHICK877 <kmass8754@gmail.com>
2024-02-22 17:00:20 +0530

    up

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
 const cors = require('cors');
 const dotenv = require('dotenv')
 dotenv.config()
