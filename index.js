b0aad9121eee6863d4887f2d640ef23205c6a742
KARTHICK877 <kmass8754@gmail.com>
2024-02-22 15:48:21 +0530

    UO

--- index.js
+++ index.js
@@ -74,6 +74,8 @@ app.post('/api/signup', async (req, res) => {
       res.status(400).send(error);
     }
   });
-  
+  app.get('/', (req, res) => {
+    res.send('form working good');
+  });
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
